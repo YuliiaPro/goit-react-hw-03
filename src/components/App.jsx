17103597ae00cc9a8bf6e8b48b75a98bd260728b
YuliiaPro <129890065+YuliiaPro@users.noreply.github.com>
2024-05-05 23:41:47 +0300

    add markup

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+//import { useState } from "react";
+// import ContactForm from "./ContactForm/ContactForm";
+// import SearchBox from "./SearchBox/SearchBox";
+// import ContactList from "./ContactList/ContactList";
+//import css "./App.module.css";
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      {/* <ContactForm />
+      <SearchBox />
+      <ContactList /> */}
+    </div>
+  );
+}
+
+export default App;
